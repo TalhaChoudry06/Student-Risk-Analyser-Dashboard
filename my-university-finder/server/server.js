1edf1e4bf8533f355d818c7724826204e571569c
Talha Choudry <talhachoudry16@gmail.com>
2025-06-30 19:51:40 -0400

    added table to output

--- my-university-finder/server/server.js
+++ my-university-finder/server/server.js
@@ -5,6 +5,13 @@ import Student from './models/student_data.js';
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend URL
+}));
+
+// parse incoming json bodies
+app.use(express.json());  
+
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/local', {
 }).then(() => {
@@ -22,16 +29,28 @@ app.listen(port, () => {
 });
 
 // Read (GET) all items
-app.post('/api/search', (req, res) => {
-    res.json({requestBody: req.body});
+app.post('/api/search', async (req, res) => {
+  try{
+    const data = req.body;
+    const major = data.major;
+    console.log(data);
+    console.log(major);
+
+    const students = await Student.find({major: major});
+    // console.log(student);
+    res.json({ success: true, students });
+  } catch (err) {
+    console.error('Error searching students by major:', err);
+    res.status(500).json({ success: false, error: 'Server error' });
+  }
 });
 
-try{
-  const student = await Student.findOne({ _id: "S002" });
-  console.log(student);
-  const unique_majors = await Student.distinct("major");
-  console.log(unique_majors)
+// try{
+//   const student = await Student.findOne({ _id: "S002" });
+//   console.log(student);
+//   const unique_majors = await Student.distinct("major");
+//   console.log(unique_majors)
 
-  } catch (err) {
-  console.error(err)
-}
+//   } catch (err) {
+//   console.error(err)
+// }
